382f105bacd61517d0505c2489d940882f206ec1
Lucas Salomao <lucastadeusalomao@gmail.com>
2025-05-22 16:12:58 -0300

    update

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,4 +1,11 @@
-from .web_search import tool as web_search_tool
-from .chatmsep import tool as chatmsep_tool
+from .chatmsep_tool import chatmsep
+from .pdf_extraction_tool import extract_full_plan_details
+from .teaching_plan_tool import generate_teaching_plan
+# from .web_search import tool as web_search_tool
 
-tools = [chatmsep_tool]
+tools = [
+    chatmsep,
+    extract_full_plan_details,
+    generate_teaching_plan,
+    # web_search_tool,
+]
